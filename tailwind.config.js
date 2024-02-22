5638af201e40980e5a968dd3966f770fc3d57239
MMOURGUES <maxime.mourgues@ynov.com>
2024-02-22 09:10:05 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ['./public/index.html', './src/**/*.vue'],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
